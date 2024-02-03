04cc7304b76f8df27199bcccd81085eeee697ef4
169kaif <kaifraushan@gmail.com>
2024-02-04 01:00:55 +0530

    add driver code and handle errors

--- q2.py
+++ q2.py
@@ -54,7 +54,7 @@ def and_not_query(postings_list, t1, t2):
 
     return answer_set
 
-def and_or_query(postings_list, t1, t2):
+def or_not_query(postings_list, t1, t2):
 
     answer_set = set()
 
@@ -70,8 +70,64 @@ def and_or_query(postings_list, t1, t2):
         for i in range(1,1000):
             if i not in postings_list[t2]:
                 answer_set.add(i)
+    else:
+
+        for i in range(1, 1000):
+            answer_set.add(i)
 
     return answer_set
+
+#or query if a file set already exists
+def or_query_fe(postings_list, t1, file_set):
+
+    #check if t1 in postings_list
+    if (t1 in postings_list.keys()):
+        for file in postings_list[t1]:
+            file_set.add(file)
+
+    return file_set
+
+#and query if a file set already exists
+def and_query_fe(postings_list, t1, file_set):
+
+    answer_file_set = set()
+
+    #check if t1 in postings list
+    if (t1 in postings_list.keys()):
+        for file in postings_list[t1]:
+            if file in file_set:
+                answer_file_set.add(file)
+
+    return answer_file_set
+
+#and not query if a file set already exists
+def and_not_query_fe(postings_list, t1, file_set):
+
+    #check if t1 in postings list
+    if (t1 in postings_list.keys()):
+        return file_set.difference(postings_list[t1])
+    
+    return file_set
+
+#and or query if a file set already exists
+def or_not_query_fe(postings_list, t1, file_set):
+
+    answer_set = set()
+
+    #check if t1 already exists
+    if (t1 in postings_list.keys()):
+        for i in range(1, 1000):
+            if i not in postings_list[t1]:
+                answer_set.add(i)
+    else:
+        for i in range(1,1000):
+            answer_set.add(i)
+
+    for file in file_set:
+        answer_set.add(file)
+
+    return answer_set
+
 
 #lowercase the text
 def lowercase_text(input_text):
@@ -202,5 +258,29 @@ for i in range(1, number_queries+1):
     queried_files = set()
     for curr_index, operation in enumerate(operations):
 
+        #iterating from left to right
+        #first operation to be run between two word
+        if (curr_index == 0):
+            if (operation == "OR"):
+                queried_files = or_query(postings_list, processed_sequence[curr_index], processed_sequence[curr_index+1])
+            elif (operation == "AND"):
+                queried_files = and_query(postings_list, processed_sequence[curr_index], processed_sequence[curr_index+1])
+            elif (operation == "AND NOT"):
+                queried_files = and_not_query(postings_list, processed_sequence[curr_index], processed_sequence[curr_index+1])
+            elif (operation == "OR NOT"):
+                queried_files = or_not_query(postings_list, processed_sequence[curr_index], processed_sequence[curr_index+1])
+        else:
+            if (operation == "OR"):
+                queried_files = or_query_fe(postings_list, processed_sequence[curr_index+1], queried_files)
+            elif (operation == "AND"):
+                queried_files = and_query_fe(postings_list, processed_sequence[curr_index+1], queried_files)
+            elif (operation == "AND NOT"):
+                queried_files = and_not_query_fe(postings_list, processed_sequence[curr_index+1], queried_files)
+            elif (operation == "OR NOT"):
+                queried_files = or_not_query_fe(postings_list, processed_sequence[curr_index+1], queried_files)
 
-        #NEED TO ADD FUNCTIONALITY FOR WHEN INSTEAD OF TWO WORDS, WE PROCESS ONE FILE SET AND QUERIED WORD FILE SET
+    print(f"Number of documents retrieved for query {i}: " + str(len(queried_files)))
+    print(f"Names of the documents retrieved for query {i}: ", end="")
+    for file_id in queried_files:
+        print("file" + str(file_id) + ".txt,", end="")
+    print("\n")
