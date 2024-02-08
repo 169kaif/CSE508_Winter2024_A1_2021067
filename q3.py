540ac816307d56b50751b3459efbfbec94fa7728
169kaif <kaifraushan@gmail.com>
2024-02-08 23:29:32 +0530

    add q3 except querying

---
+++ q3.py
@@ -0,0 +1,138 @@
+import os
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def extract_file_id(file_name):
+    return int(file_name[4:-4])
+
+#lowercase the text
+def lowercase_text(input_text):
+    return input_text.lower()
+
+#tokenize the text
+def tokenize_text(input_text):
+    return word_tokenize(input_text)
+
+#remove stop words from the text
+def remove_stopwords(input_text):
+
+    stop_words = set(stopwords.words('english'))
+
+    #store words except for the stop words
+    cleaned_words = []
+
+    for word in input_text:
+        if word not in stop_words:
+            cleaned_words.append(word)
+
+    return cleaned_words
+
+#remove punctuation marks
+def remove_punctuation(input_text):
+
+    #init array to store words that are not punctuation
+    np_words = []
+
+    for word in input_text:
+        new_word = ""
+        for char in word:
+            if (char.isalnum()):
+                new_word += char
+        if (len(new_word) > 0):
+            np_words.append(new_word)
+
+    return np_words
+
+#remove blank space tokens
+def remove_blankspace(input_text):
+
+    #init array to store words without blankspace
+    bl_words = []
+
+    for word in input_text:
+        new_word = "".join(word.split())
+        bl_words.append(new_word)
+
+    return bl_words
+
+#apply preprocessing functions to the text
+def preprocess_text(input_text):
+
+    #lowercase the text
+    input_text = lowercase_text(input_text)
+
+    #tokenize the text
+    input_text = tokenize_text(input_text)
+
+    #remove stopwords
+    input_text = remove_stopwords(input_text)
+
+    #remove punctuations
+    input_text = remove_punctuation(input_text)
+
+    #remove blank space tokens
+    input_text = remove_blankspace(input_text)
+
+    return input_text
+
+#----------------------main---------------------#
+
+#init postings list
+postings_list = {}
+
+for file in os.listdir("preprocessed_files"):
+
+    #handling hidden files
+    if (file[0] == '.'):
+        continue
+
+    file_read_path = "preprocessed_files" + "/" + file
+    file_id = extract_file_id(file)
+
+    curr_word_ind = 0
+
+    with open(file_read_path, 'r') as curr_file:
+        for line in curr_file:
+            curr_word = line.strip()
+            if curr_word not in postings_list:
+                postings_list[curr_word] = {file_id: [curr_word_ind]}
+            else:
+                if file_id in postings_list[curr_word]:
+                    postings_list[curr_word][file_id].append(curr_word_ind)
+                else:
+                    postings_list[curr_word][file_id] = [curr_word_ind]
+
+            curr_word_ind += 1
+
+#specifying dump file path
+dump_file_path = "./positional_index.pk1"
+
+#saving the unigram inverted index in a dump file
+with open(dump_file_path, 'wb') as f:
+    pickle.dump(postings_list, f)
+
+#---------query processing-----------#
+    
+#collect number of queries as input
+number_queries = int(input())
+
+for i in range(1, number_queries+1):
+
+    #input query
+    input_sequence = input()
+
+    #preprocess input_sequence
+    processed_sequence = preprocess_text(input_sequence)
+
+    #set to store the queried files
+    queried_files = set()
+
+    #PROCESS QUERIES
+    
+
+    print(f"Number of documents retrieved for query {i}: " + str(len(queried_files)))
+    print(f"Names of the documents retrieved for query {i}: ", end="")
+    for file_id in queried_files:
+        print("file" + str(file_id) + ".txt,", end="")
+    print("\n")
